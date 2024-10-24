b956106828e9fbe2e48cc33c911323a67c4aa726
codeliveyou <codetankstn@gmail.com>
2024-10-24 16:54:01 -0400

    'requirements'

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ app.add_middleware(
 
 app.include_router(router, prefix="/api")
 
-if __name__=="__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__=="__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
